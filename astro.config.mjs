5fbd0c8631d0bfe937732c8cc91154c01f1b57e9
Davide Aimar <davide.aimar99@gmail.com>
2024-01-18 23:01:16 +0100

    add: 404, sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://www.tedxcuneo.com",
+  integrations: [tailwind(), sitemap()],
 });
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,6 @@
+import type { TypePageSkeleton } from "./generated/contentful";
+import type { Entry } from "./types";
+
 /**
  * Formats a date as a string in the format "HH:MM:SS"
  *
@@ -18,3 +21,50 @@ export function formatTimestamp(date: Date): string {
   })}`;
   return ts;
 }
+
+/**
+ * A fake page coming from Contentful
+ */
+export const fakePage: Entry<TypePageSkeleton> = {
+  sys: {
+    id: "page",
+    type: "Entry",
+    contentType: {
+      sys: {
+        type: "Link",
+        linkType: "ContentType",
+        id: "page",
+      },
+    },
+    createdAt: "2021-08-31T14:00:00.000Z",
+    updatedAt: "2021-08-31T14:00:00.000Z",
+    revision: 1,
+    space: {
+      sys: {
+        type: "Link",
+        linkType: "Space",
+        id: "space",
+      },
+    },
+    environment: {
+      sys: {
+        id: "master",
+        type: "Link",
+        linkType: "Environment",
+      },
+    },
+  },
+  metadata: {
+    tags: [],
+  },
+  fields: {
+    title: "Page",
+    slug: "page",
+    internalTitle: "",
+    layout: "Generico",
+    ogImage: undefined,
+    ogTitle: "",
+    noIndex: true,
+    noFollow: true,
+  },
+};
