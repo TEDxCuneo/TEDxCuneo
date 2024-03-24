6c534f73a68c061d82866d14a4941c526c904890
Davide Aimar <davide.aimar99@gmail.com>
2024-03-24 17:31:23 +0100

    lint & bump version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-import robotsTxt from "astro-robots-txt";
+import robotsTxt from 'astro-robots-txt';
 import tailwindcssNesting from 'tailwindcss/nesting';
 
 export default defineConfig({
-  site: 'https://www.tedxcuneo.com',
-  integrations: [tailwind(), sitemap(), robotsTxt()],
-  vite: {
-    css: {
-      postcss: {
-        plugins: [tailwindcssNesting()]
-      }
-    }
-  }
+	site: 'https://www.tedxcuneo.com',
+	integrations: [tailwind(), sitemap(), robotsTxt()],
+	vite: {
+		css: {
+			postcss: {
+				plugins: [tailwindcssNesting()],
+			},
+		},
+	},
 });
