f4fe4175d31e14c8b19a15ffd45ca8e867e79d92
Davide Aimar <davide.aimar99@gmail.com>
2025-03-28 00:04:24 +0100

    WIP: draggable stickers

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,17 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import robotsTxt from 'astro-robots-txt';
 import tailwindcssNesting from 'tailwindcss/nesting';
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://www.tedxcuneo.com',
-	integrations: [tailwind(), sitemap(), robotsTxt()],
-	vite: {
-		css: {
-			postcss: {
-				plugins: [tailwindcssNesting()],
-			},
-		},
-	},
+  site: 'https://www.tedxcuneo.com',
+  integrations: [tailwind(), sitemap(), robotsTxt(), react()],
+  vite: {
+    css: {
+      postcss: {
+        plugins: [tailwindcssNesting()]
+      }
+    }
+  }
 });
---
+++ src/components/DraggableImg.tsx
@@ -0,0 +1,42 @@
+import { motion } from "framer-motion";
+
+type InputProps = {
+    src: string;
+    alt: string;
+    className?: string;
+}
+
+export default function DraggableImg({ src, alt, className }: InputProps) {
+    return (
+        <motion.img
+            drag
+            dragElastic={0.7}
+            dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+            
+            initial={{ scale: 1 }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.95 }}
+            whileDrag={{ 
+                scale: 1.1,
+                cursor: 'grabbing'
+            }}
+            
+            animate={{
+                scale: 1.1,
+                transition: {
+                    type: "spring",
+                    stiffness: 300,
+                    damping: 10,
+                    bounce: 1
+                }
+            }}
+            
+            src={src}
+            alt={alt}
+            className={`cursor-grab ${className || ''}`}
+            style={{ 
+                touchAction: 'none'
+            }}
+        />
+    );
+}
